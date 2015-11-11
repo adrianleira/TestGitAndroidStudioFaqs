a15fb12de2a2abb56f97d0ab9ac5b2f84be23d27
adrian_leira_carro <adrianleiracarro@gmail.com>
2015-11-11 20:42:29 +0100

    Modificaciones

--- app/src/main/java/es/aadesigners/testgitandroidstudiofaqs/MainActivity.java
+++ app/src/main/java/es/aadesigners/testgitandroidstudiofaqs/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        nuevaFuncionalidad();
     }
 
     public void nuevaFuncionalidad(){
